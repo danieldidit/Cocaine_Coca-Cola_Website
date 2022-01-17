2a4bff255b5b0a8447f4c01defa1595dc6410a00
Daniel Humphreys <danielhumphreys@live.ca>
2022-01-17 17:17:41 -0500

    Initial commit

---
+++ js/custom-js.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+  //Loads in the header
+  $(function(){
+    $("#navigation").load("header.html");
+  });
+  //Loads in the footer
+  $(function(){
+    $("#footer").load("footer.html");
+  });
+
+});
